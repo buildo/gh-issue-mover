1b5005099a8ba4b48d9563750a0ec368f2d08f7a
Gabriele Petronella <gabriele@buildo.io>
2016-09-13 17:05:35 +0300

    Migrate comments too, exclude PRs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Octokat from 'octokat';
 import inquirer from 'inquirer';
 import 'colors';
 import open from 'open';
-import { omit } from 'lodash';
+import { pick } from 'lodash';
 import { fromRepo as fromRepoConfig, toRepo as toRepoConfig } from './config.json';
 
 const fromGithub = new Octokat({
@@ -18,59 +18,97 @@ const toGithub = new Octokat({
 const fromRepo = fromGithub.repos(fromRepoConfig.owner, fromRepoConfig.name);
 const toRepo = fromGithub.repos(toRepoConfig.owner, toRepoConfig.name);
 
+async function migrateComment(issue, comment) {
+  const authorshipNote = [
+    '---',
+    '',
+    `Migrated from ${comment.htmlUrl}`,
+    `Originally created by @${comment.user.login} on *${new Date(comment.createdAt).toUTCString()}*`,
+    '',
+    '---'
+  ].join('\n');
+  const commentToCreate = {
+    body: `${authorshipNote}\n${comment.body}`
+  };
+  await toRepo.issues(issue.number).comments.create(commentToCreate);
+}
 
 async function migrateIssue(issue) {
   const authorshipNote = [
     '---',
     '',
-    `Migrated from ${fromRepoConfig.owner}/${fromRepoConfig.name}#${issue.number}`,
+    `Migrated from ${issue.htmlUrl}`,
     `Originally created by @${issue.user.login} on *${new Date(issue.createdAt).toUTCString()}*`,
     '',
     '---'
   ].join('\n');
   const issueToCreate = {
-    ...omit(issue, ['assignee']),
+    ...pick(issue, ['title', 'labels']),
+    assignees: issue.assignees.map(a => a.login),
     body: `${authorshipNote}\n${issue.body}`
   };
-  const newIssue = await toRepo.issues.create(issueToCreate);
-  await fromRepo.issues(issueNumber).comment.create({
-    body: `Issue migrated to ${toRepoConfig.owner}/${toRepoConfig.name}#${newIssue.number}`
-  });
-  await fromRepo.issues(issueNumber).update({ state: 'closed' });
-  console.log(
-    '\n',
-    '🍭  Successfully migrated issue',
-    'from',
-    `${fromRepoConfig.owner}/${fromRepoConfig.name}#${issue.number}`.bold,
-    'to',
-    `${toRepoConfig.owner}/${toRepoConfig.name}#${newIssue.number}`.bold
-  );
-  return newIssue;
+  try {
+    const newIssue = await toRepo.issues.create(issueToCreate);
+    const comments = await fromRepo.issues(issue.number).comments.fetch();
+    await Promise.all(comments.map(c => migrateComment(newIssue, c)));
+    await fromRepo.issues(issue.number).comments.create({
+      body: `Issue migrated to ${toRepoConfig.owner}/${toRepoConfig.name}#${newIssue.number}`
+    });
+    await fromRepo.issues(issue.number).update({ state: 'closed' });
+    console.log(
+      '\n',
+      '🍭  Successfully migrated issue',
+      'from',
+      `${fromRepoConfig.owner}/${fromRepoConfig.name}#${issue.number}`.bold,
+      'to',
+      `${toRepoConfig.owner}/${toRepoConfig.name}#${newIssue.number}`.bold,
+      '\n'
+    );
+    return newIssue;
+  } catch (e) {
+    console.log(`😱 Something went wrong while migrating issue #${issue.number}!`);
+    console.log(JSON.stringify(e, null, 2));
+  }
 }
 
 async function migrateIssuesByLabel(labels) {
-  const issues = await fromRepo.issues.fetch({ labels, state: 'open' });
-  const { confirm } = await inquirer.prompt([{
-    type: 'confirm',
-    name: 'confirm',
-    default: false,
-    message: [
-      `You're about to migrate`,
-      `${issues.length} issues`.green,
-      'matching the labels',
-      labels.green
-    ].join(' ')
-  }]);
-  if (confirm) {
-    await Promise.all(issues.forEach(migrateIssue));
-    console.log(
-      '\n',
-      `🌟  Successfully migrated ${issues.length} issues`,
-      'from',
-      `${fromRepoConfig.owner}/${fromRepoConfig.name}`.bold,
-      'to',
-      `${toRepoConfig.owner}/${toRepoConfig.name}`.bold
-    );
+  const issuesAndPRs = await fromRepo.issues.fetch({ labels, state: 'open', filter: 'all' });
+  const issues = issuesAndPRs.filter(i => !pull_request);
+
+  if (issues.length === 0) {
+    console.log(`Sorry, no issues found matching labels ${labels.split(', ').map(l => l.green)}`);
+    const { retry } = await inquirer.prompt([{
+      type: 'confirm',
+      name: 'retry',
+      message: 'Do you want to try again?'
+    }]);
+    if (retry) {
+      await migrateIssues();
+    }
+  } else {
+    const { confirm } = await inquirer.prompt([{
+      type: 'confirm',
+      name: 'confirm',
+      default: false,
+      message: [
+        `You're about to migrate`,
+        `${issues.length} issues`.green,
+        'matching the labels',
+        labels.split(', ').map(l => l.green),
+        '. Are you sure?'
+      ].join(' ')
+    }]);
+    if (confirm) {
+      await Promise.all(issues.forEach(migrateIssue));
+      console.log(
+        '\n',
+        `🌟  Successfully migrated ${issues.length} issues`,
+        'from',
+        `${fromRepoConfig.owner}/${fromRepoConfig.name}`.bold,
+        'to',
+        `${toRepoConfig.owner}/${toRepoConfig.name}`.bold
+      );
+    }
   }
 }
 
@@ -155,22 +193,23 @@ async function chooseMigrationType() {
   return { migrationType, labels };
 }
 
-async function main() {
-  process.stdout.write('\x1Bc');
-
-  console.log('🖖  Greetings, hooman!\n')
-
+async function migrateIssues() {
   const { migrationType, labels } = await chooseMigrationType();
 
   switch (migrationType) {
     case 'byLabel': await migrateIssuesByLabel(labels); break;
     case 'oneByOne': await migrateIssuesOneByOne(); break;
   }
+}
 
+async function main() {
+  await migrateIssues();
   console.log('\n👋  Ok! Goodbye!'.bold);
 }
 
 try {
+  process.stdout.write('\x1Bc');
+  console.log('🖖  Greetings, hooman!\n')
   main();
 } catch (e) {
   console.log(e);
